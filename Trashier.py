9b1ef048852e81b6ae8f4bfe11b46097c4693c66
LegoNinja39 <legoninja13@sbcglobal.net>
2023-04-01 12:24:11 -0700

    modified:   Trashier.py

--- Trashier.py
+++ Trashier.py
@@ -1,7 +1,21 @@
 s = "   "
 ss = ""
 
-x = lambda y: print("Yes") if not y or y.isspace() else print("No")
+
+def x(y): return print("Yes") if not y or y.isspace() else print("No")
+
 
 x(s)
 x(ss)
+
+
+def tHrInc(dataLen: int) -> None:
+    for hr in range(dataLen):
+        if hr > 42:
+            activeHr = (hr - 42) * 3 + 42
+        else:
+            activeHr = hr
+        print(activeHr)
+
+
+tHrInc(70-1-1)
